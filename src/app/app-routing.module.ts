3e3203a8b430dd6f5d34608134a457038397c9b3
Mahendra Bagul <mahendrahiraman.bagul@team.telstra.com>
2018-11-29 22:41:18 +0530

    Login working fine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,49 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {AuthGuardService} from './services/auth-guard/auth-guard.service';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {HomeComponent} from './components/home/home.component';
+import {GetDeviceComponent} from './components/get-robot/get-device.component';
+import {FindProductComponent} from './components/find-product/find-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent,
+  },
+  {
+    path: 'find-product',
+    component: FindProductComponent,
+    data: {title: 'Find Product'},
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'app-home',
+    component: HomeComponent,
+    data: {title: 'Home'},
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'get-device',
+    component: GetDeviceComponent,
+    data: {title: 'Get Device'},
+    canActivate: [AuthGuardService]
+  },
+
+  {
+    path: '',
+    redirectTo: '/app-home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
 import {FindProductComponent} from './components/find-product/find-product.component';
@@ -18,21 +14,31 @@ import {ErrorInterceptor} from './helpers/error-interceptor';
 import {JwtInterceptor} from './helpers/jwt-interceptor';
 import {TenantService} from './services/tenant/tenant.service';
 import {DeviceService} from './services/device/device.service';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import {CustomMaterialModule} from './material.module';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {LoginComponent} from './components/login/login.component';
+import {HomeComponent} from './components/home/home.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
     FindProductComponent,
-    GetDeviceComponent
+    GetDeviceComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
+    CustomMaterialModule, MatSelectModule,
+    ReactiveFormsModule, FormsModule,
     BrowserModule,
     HttpClientModule,
+    NgxSpinnerModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatButtonModule,
     ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
